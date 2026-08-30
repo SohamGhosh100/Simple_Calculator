740dd00fdacdef8029321e75f5ea4654a7f5461b
SohamGhosh100 <sohamghosh2072@gmail.com>
2026-08-30 21:46:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let display = document.getElementById("display");
+
+function input(value) {
+    if (display.innerText == "0") {
+        display.innerText = value;
+    } else {
+        display.innerText += value;
+    }
+}
+
+function clearDisplay() {
+    display.innerText = "0";
+}
+
+function deleteNumber() {
+    display.innerText = display.innerText.slice(0, -1) || "0";
+}
+
+function calculate() {
+    try {
+        display.innerText = eval(display.innerText.replace("^", "**"));
+    } catch {
+        display.innerText = "Error";
+    }
+}
